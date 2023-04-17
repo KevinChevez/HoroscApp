d396e235b599013cd8233b2b4ac56d502c296816
KevinChevez <kchevez@espol.edu.ec>
2023-04-17 14:32:36 -0500

    Implementation of Hilt to Inject dependencies completed.

---
+++ app/src/main/java/com/example/horoscapp/ui/information/InformationViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.horoscapp.ui.information
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class InformationViewModel @Inject constructor(): ViewModel() {
+}
---
+++ app/src/main/java/com/example/horoscapp/ui/list/ListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.horoscapp.ui.list
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class ListViewModel @Inject constructor(): ViewModel() {
+}
